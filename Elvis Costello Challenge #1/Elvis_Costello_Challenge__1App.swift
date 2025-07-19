64f02550bef9fff16b267338bf7637ca9b16cbd9
Jr Christopher <jrtt212@gmail.com>
2025-07-19 11:06:22 +1000

    Initial Commit

---
+++ Elvis Costello Challenge #1/Elvis_Costello_Challenge__1App.swift
@@ -0,0 +1,17 @@
+//
+//  Elvis_Costello_Challenge__1App.swift
+//  Elvis Costello Challenge #1
+//
+//  Created by Jedda Tuuta on 19/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct Elvis_Costello_Challenge__1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
